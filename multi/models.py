ff701cf84c6fb7fd4727d8ae6fddf47ffc95d436
daniel amodu <danielolayinka97@gmail.com>
2023-03-03 16:56:55 +0100

    .

--- multi/models.py
+++ multi/models.py
@@ -12,7 +12,7 @@ class Todo(models.Model):
         ordering =('-date_added',)
 
 class Dic(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     word = models.CharField(max_length=255,null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -23,7 +23,7 @@ class Dic(models.Model):
         ordering  = ['-date_added']
 
 class City(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
